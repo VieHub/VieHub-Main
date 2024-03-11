e2890ca8150190ff4057864053dd889cb6587cdb
Ahmed14z <ahmedeslam1582002@gmail.com>
2024-03-11 16:34:52 +0200

    remove supabase

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AuthProvider from "./hooks/AuthProvider.tsx";
+// import AuthProvider from "./hooks/AuthProvider.tsx";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,9 +15,7 @@ export const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 );
--- client/src/modules/auth/pages/Login.tsx
+++ client/src/modules/auth/pages/Login.tsx
@@ -1,16 +1,12 @@
-import supabase from "../../../api/supabase";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-
 const Login = () => (
   <div className="mt-20 h-full w-full p-5 md:p-10">
     <div className="mx-auto max-w-md">
-      <Auth
+      {/* <Auth
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}
         providers={["google", "discord", "github"]}
         redirectTo={window.location.host}
-      />
+      /> */}
     </div>
   </div>
 );
