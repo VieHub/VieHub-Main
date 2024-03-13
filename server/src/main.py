5e08d9e9927879ef16650f24e5a5966b00a7c9b6
Ahmed14z <ahmedeslam1582002@gmail.com>
2024-03-13 12:11:00 +0200

    q

--- server/src/main.py
+++ server/src/main.py
@@ -28,7 +28,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Vie Hub",
     description="""
-    Auth routes here are just for debugging
+    Routes here are just for debugging
         """,
 )
 
