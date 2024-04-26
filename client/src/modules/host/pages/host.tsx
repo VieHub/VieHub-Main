f0590e594a72425b7496fa31ec4abdded99acba4
Omar <omaraboulmaaty.11@gmail.com>
2024-04-26 22:33:03 +0300

    add fisrt div in the page

--- client/src/modules/host/pages/host.tsx
+++ client/src/modules/host/pages/host.tsx
@@ -8,34 +8,43 @@ const Host: React.FC = () => {
     return (
         <div className="h-full w-full">
             <Header isLoggedin={true} />
-            <div className="host-container">
-                {/* <div className="carousel"> */}
-                    <img src={Image} alt=""  className="carousel-img"/>
-                {/* </div> */}
+            <div className="host-container relative">
+                <img src={Image} alt="" className="carousel-img" />
                 <div className="carousel text-white">
                     <div id="item1" className="carousel-item w-full ">
-                        <h1 className=" mb-2 text-xl font-bold text-black" style={{ color: "white" }}>
-                        Public competition
+                        <h1 className="mb-2 text-xl font-bold text-black" style={{ color: "white" }}>
+                            Public competition
                         </h1>
-                        <p className=" text-base" style={{ color: "white" }}>
-                          Host vibrant public competitions effortlessly with our platform.
-                          Engage diverse participants and showcase talents seamlessly. Start hosting now!
+                        <p className="text-base" style={{ color: "white" }}>
+                            Host vibrant public competitions effortlessly with our platform,
+                            Engage diverse participants and showcase talents seamlessly.
                         </p>
+                        <button className="host-btn px-4 py-2 text-sm font-medium text-white shadow-sm ">
+                            Host
+                        </button>
                     </div>
                     <div id="item2" className="carousel-item w-full">
-                          <h1 className=" mb-2 text-xl font-bold text-black" style={{ color: "white" }}>
+                        <h1 className="mb-2 text-xl font-bold text-black" style={{ color: "white" }}>
                             Private competition
-                          </h1>
-                          <p className=" text-base" style={{ color: "white" }}>
-                            hosts a discreet and exclusive environment for tailored contests.
+                        </h1>
+                        <p className=" " style={{ color: "white" }}>
+                            Hosts a discreet and exclusive environment for tailored contests.
                             Accessible through unique links, these competitions provide a more controlled and targeted experience
-                          </p>
+                        </p>
+                        <button className="host-btn px-4 py-2 text-sm font-medium text-white shadow-sm ">
+                            Host
+                        </button>
                     </div>
                 </div>
-                <div className="swap flex justify-center py-2 gap-2">
-                    <a href="#item1" className="btn btn-xs">1</a>
-                    <a href="#item2" className="btn btn-xs">2</a>
-                </div>
+
+                    <div className="swap absolute bottom-10 left-100 right-8 flex flex-column items-center py-2 gap-2">
+                        <div onClick={() => window.location.href="#item1"} className="btn btn-circle"></div>
+                        <div onClick={() => window.location.href="#item2"} className="btn btn-circle"></div>
+                    </div>
+                {/* <div className="circles absolute bottom-10 left-0 right-8 flex flex-column items-center py-2 gap-2">
+                    <div onClick={() => window.location.href="#item1"} className="circle"></div>
+                    <div onClick={() => window.location.href="#item2"} className="circle"></div>
+                </div> */}
             </div>
             {/* <Footer /> */}
         </div>
@@ -43,3 +52,4 @@ const Host: React.FC = () => {
 };
 
 export default Host;
+
