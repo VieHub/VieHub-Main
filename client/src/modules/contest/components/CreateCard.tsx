959d9dd667ee2efb5be90ee1bc18b2b039910121
Ahmed14z <ahmedeslam1582002@gmail.com>
2024-05-11 00:43:40 +0300

    feat: update contest page logic

--- client/src/modules/contest/components/CreateCard.tsx
+++ client/src/modules/contest/components/CreateCard.tsx
@@ -5,38 +5,52 @@ interface ContestCardProps {
   description: string;
   image: string;
   prize: string;
-  participants: string;
+  participants: any;
   companyName: string;
   startDate: string;
   endDate: string;
 }
 
-const ContestCard: React.FC<ContestCardProps> = ({ 
-  name, 
-  description, 
-  image, 
-  prize, 
-  participants, 
-  companyName, 
-  startDate, 
-  endDate 
+const ContestCard: React.FC<ContestCardProps> = ({
+  name,
+  description,
+  image,
+  prize,
+  participants,
+  companyName,
+  startDate,
+  endDate,
 }) => {
   return (
-    <div className=" contest-card max-w-4xl w-full rounded-lg overflow-hidden shadow-lg mx-4 my-4 flex ">
-      <img className="w-1/4 h-50 object-cover" src={image} alt={name} />
-      <div className="w-3/4 px-6 py-4 flex flex-col" style={{ backgroundColor: '#2B6777' }}>
-        <div className="font-bold text-xl mb-2 text-white">{name}</div>
+    <div className=" contest-card mx-4 my-4 flex w-full max-w-4xl overflow-hidden rounded-lg shadow-lg ">
+      <img className="h-50 w-1/4 object-cover" src={image} alt={name} />
+      <div
+        className="flex w-3/4 flex-col px-6 py-4"
+        style={{ backgroundColor: "#2B6777" }}
+      >
+        <div className="mb-2 text-xl font-bold text-white">{name}</div>
         <p className="text-gray-700 text-base text-white">{description}</p>
         <hr className="my-4 border-white" />
         <div className="flex justify-between text-white">
           <div>
-            <p><span className="font-bold">Prize:</span> {prize}</p>
-            <p><span className="font-bold">Participants:</span> {participants}</p>
+            <p>
+              <span className="font-bold">Prize:</span> {prize}
+            </p>
+            <p>
+              <span className="font-bold">Participants:</span>{" "}
+              {participants.length}
+            </p>
           </div>
           <div>
-            <p><span className="font-bold">Company:</span> {companyName}</p>
-            <p><span className="font-bold">Start Date:</span> {startDate}</p>
-            <p><span className="font-bold">End Date:</span> {endDate}</p>
+            <p>
+              <span className="font-bold">Company:</span> {companyName}
+            </p>
+            <p>
+              <span className="font-bold">Start Date:</span> {startDate}
+            </p>
+            <p>
+              <span className="font-bold">End Date:</span> {endDate}
+            </p>
           </div>
         </div>
       </div>
