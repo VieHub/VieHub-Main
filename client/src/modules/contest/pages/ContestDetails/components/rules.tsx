68275ea6583a8c4faea6d3f34dfac78d5811a3d8
Omar <omaraboulmaaty.11@gmail.com>
2024-05-14 19:55:05 +0300

    add-overview-rules-schedule

---
+++ client/src/modules/contest/pages/ContestDetails/components/rules.tsx
@@ -0,0 +1,137 @@
+// import React from "react";
+
+
+
+
+
+const Rules = () => {
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="forth-details-section mt-12 mb-8">
+        <div className="mt-12 flex flex-row items-center">
+          <h1 className="requierment ">Project and Submission Requirements:</h1>
+          
+        </div>
+        <div className="lis-req flex flex-col mt-6">
+          <li className="font-bold">
+            programming Proficiency:
+            <span className="font-normal">Demonstrate strong skills in JavaScript and proficiency in Node.js programming</span>
+          </li>  
+          <li className="font-bold mt-6">
+          Web Development Knowledge:
+          <span className="font-normal">Have a foundational understanding of key web development concepts, 
+          including but not limited to, server-side scripting, asynchronous programming, and RESTful APIs.
+          </span>
+        </li>    
+        <li className="font-bold mt-6">
+          Code Editor:
+          <span className="font-normal mt-6">Utilize a preferred code editor to participate in the competition.
+          Whether it's VS Code, Sublime Text, or any other, make sure you're comfortable with your chosen tool.
+          </span>
+        </li>    
+        <li className="font-bold mt-6">
+        Stable Internet Connection: 
+          <span className="font-normal">Ensure a reliable and stable internet connection throughout the competition to facilitate seamless participation. </span>
+        </li>  
+                  
+        </div>
+        
+        <div className=" mt-12 flex flex-row items-center">
+          <h1 className="requierment ">PRIZES</h1>
+          <hr className="border-t border-gray-400 ml-6 section-line opacity-3" />
+        </div>
+        <div className="prize lis-req flex mt-6 ">
+          <div className="mr-12">
+            <span className="font-bold mt-6">
+              First Place:
+            <li className="font-normal">
+              $500 cash prize
+            </li>
+            <li className="font-normal">
+              an exclusive trophy
+            </li>
+            <li className="font-normal">
+              certificate of excellence.
+            </li>
+            </span>
+            <span className="font-bold mt-6">
+            Second Place:
+            <li className="font-normal">
+              $300 cash prize
+            </li>
+            <li className="font-normal">
+            cutting-edge
+            </li>
+            <li className="font-normal">
+            certificate of achievement.
+            </li>
+            </span>
+            <span className="font-bold mt-6">
+            Innovation Award:
+            <li className="font-normal">
+              $100 cash prize
+            </li>
+            <li className="font-normal">
+            certificate for the most innovative solution
+            </li>
+            <li className="font-normal">
+            a technology workshop voucher
+            </li>
+            </span>
+        </div>
+        <div className="">
+        <span className="font-bold mt-6">
+        People's Choice: 
+            <li className="font-normal">
+              $100 cash prize
+            </li>
+            <li className="font-normal">
+            voted by participants
+            </li>
+            <li className="font-normal">
+            certificate and special recognition.
+            </li>
+            </span>
+            <span className="font-bold mt-6">
+              Honorable Mentions: 
+            <li className="font-normal">
+            Certificates for outstanding contributions and a curated selection of coding resources
+            </li>
+            </span>
+        </div>
+             
+        </div>
+
+        <div className="mt-12 flex flex-row items-center">
+          <h1 className="requierment ">JUDGING CRITERIA</h1>
+          <hr className="border-t border-gray-400 ml-6 section-line opacity-3" />
+        </div>
+        <div className="lis-req flex flex-col mt-6">
+          <li className="font-bold">
+            programming Proficiency:
+            <span className="font-normal">Demonstrate strong skills in JavaScript and proficiency in Node.js programming</span>
+          </li>  
+          <li className="font-bold mt-6">
+          Web Development Knowledge:
+          <span className="font-normal">Have a foundational understanding of key web development concepts, 
+          including but not limited to, server-side scripting, asynchronous programming, and RESTful APIs.
+          </span>
+        </li>    
+        <li className="font-bold mt-6">
+          Code Editor:
+          <span className="font-normal mt-6">Utilize a preferred code editor to participate in the competition.
+          Whether it's VS Code, Sublime Text, or any other, make sure you're comfortable with your chosen tool.
+          </span>
+        </li>    
+        <li className="font-bold mt-6">
+        Stable Internet Connection: 
+          <span className="font-normal">Ensure a reliable and stable internet connection throughout the competition to facilitate seamless participation. </span>
+        </li>     
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Rules;
