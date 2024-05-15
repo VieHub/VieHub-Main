583740e39a5e22f31eada7e6573a5325f55f6330
Ahmed14z <ahmedeslam1582002@gmail.com>
2024-05-15 14:32:57 +0300

    fix: fix contests page display issue

--- client/src/modules/contest/pages/contest.tsx
+++ client/src/modules/contest/pages/contest.tsx
@@ -177,11 +177,11 @@ const Contest = () => {
                   name: string;
                   description: string;
                   image: string;
-                  prize: string;
+                  prizeDetails: string;
                   participants: any;
                   company: string;
-                  start_date: string;
-                  end_date: string;
+                  startDate: string;
+                  endDate: string;
                   image_url: string;
                 },
                 index: Key | null | undefined,
@@ -192,11 +192,11 @@ const Contest = () => {
                   name={contest.name}
                   description={contest.description}
                   image={contest.image_url}
-                  prize={contest.prize}
+                  prize={contest.prizeDetails}
                   participants={contest.participants}
                   companyName={contest.company}
-                  startDate={contest.start_date}
-                  endDate={contest.end_date}
+                  startDate={contest.startDate}
+                  endDate={contest.endDate}
                 />
               ),
             )}
