b7c3f09c62c6c0edfcde4633177c7af44d5dae1e
Ahmed14z <ahmedeslam1582002@gmail.com>
2024-06-18 15:39:26 +0300

    feat: display contests inside profile

--- client/src/modules/profile/pages/component/contest.tsx
+++ client/src/modules/profile/pages/component/contest.tsx
@@ -1,37 +1,58 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 import image from "@/assets/images/Problem-Solving.jpg";
+import { contestDetailsData } from "@/hooks/contests/useGetContestDetails";
 
 const ProfileContest: React.FC = () => {
-  return (
-    <div className="profile-contest mt-4 flex w-1/4 flex-col items-center md:flex-1 ">
-      <div className="contest-card profile-contest mx-4 my-4  flex overflow-hidden shadow-lg md:rounded-lg">
-        <img
-          className="object-cover md:h-full md:w-1/4"
-          src={image}
-          alt="Contest Image"
-        />
-        <div
-          className="flex w-full flex-col px-6 py-4 md:w-3/4"
-          style={{ backgroundColor: "#2B6777" }}
-        >
-          <div className="mb-2 text-xl font-bold text-white">
-            Node js Contest
-          </div>
+  const { contests } = useOutletContext<{ contests: string[] }>();
 
-          <hr className="my-4 border-white" />
-          <div className="flex justify-between text-white">
-            <div>
-              <p>
-                <span className="font-bold">Prize:</span> 500$
-              </p>
-              <p>
-                <span className="font-bold">Participants:</span>
-                {3}
-              </p>
+  return (
+    <div className="profile-contests-container">
+      {contests?.map((contestId) => (
+        <SingleContest key={contestId} contestId={contestId} />
+      ))}
+    </div>
+  );
+};
+
+const SingleContest: React.FC<{ contestId: string }> = ({ contestId }) => {
+  const { data, error } = contestDetailsData(contestId);
+
+  return (
+    <div className="profile-contest mt-4 flex w-1/4 flex-col items-center md:flex-1">
+      {error ? (
+        <div>Error loading contest details</div>
+      ) : !data ? (
+        <div>Loading contest details...</div>
+      ) : (
+        <div className="contest-card profile-contest mx-4 my-4 flex overflow-hidden shadow-lg md:rounded-lg">
+          <img
+            className="object-cover md:h-full md:w-1/4"
+            src={data.image_url || image}
+            alt="Contest Image"
+          />
+          <div
+            className="flex w-full flex-col px-6 py-4 md:w-3/4"
+            style={{ backgroundColor: "#2B6777" }}
+          >
+            <div className="mb-2 text-xl font-bold text-white">
+              {data.title}
+            </div>
+            <hr className="my-4 border-white" />
+            <div className="flex justify-between text-white">
+              <div>
+                <p>
+                  <span className="font-bold">Prize:</span> {data.prizeDetails}
+                </p>
+                <p>
+                  <span className="font-bold">Participants:</span>{" "}
+                  {data.participants ? data.participants.length : "N/A"}
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
